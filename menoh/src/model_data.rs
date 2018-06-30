5d2ebcb6a79cc5f6ed35cb23fe0c621651142151
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-01 05:08:25 +0900

    init with null

--- menoh/src/model_data.rs
+++ menoh/src/model_data.rs
@@ -2,6 +2,7 @@ use menoh_sys;
 use std::ffi;
 use std::mem;
 use std::path;
+use std::ptr;
 
 use error::check;
 use Error;
@@ -16,7 +17,7 @@ impl ModelData {
     {
         let path = ffi::CString::new(path.as_ref().as_os_str().to_string_lossy().as_ref())
             .map_err(|_| Error::InvalidFilename)?;
-        let mut model_data = unsafe { mem::uninitialized() };
+        let mut model_data = ptr::null_mut();
         unsafe {
             check(menoh_sys::menoh_make_model_data_from_onnx(path.as_ptr(), &mut model_data))?;
         }
