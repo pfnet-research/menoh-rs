6438c7a7dca742fa3d9997834ffe7122cba3b846
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-03 04:09:18 +0900

    remove test_ prefix

--- menoh/src/model_data/tests.rs
+++ menoh/src/model_data/tests.rs
@@ -3,13 +3,13 @@ extern crate mktemp;
 use super::ModelData;
 
 #[test]
-fn test_from_onnx() {
+fn from_onnx() {
     let file = mktemp::Temp::new_file().unwrap();
     ModelData::from_onnx(file).unwrap();
 }
 
 #[test]
 #[should_panic(expected="InvalidFilename")]
-fn test_from_onnx_invalid_path() {
+fn from_onnx_invalid_path() {
     ModelData::from_onnx("invalid.onnx").unwrap();
 }
--- menoh/src/variable_profile_table_builder/tests.rs
+++ menoh/src/variable_profile_table_builder/tests.rs
@@ -1,12 +1,12 @@
 use super::VariableProfileTableBuilder;
 
 #[test]
-fn test_new() {
+fn new() {
     VariableProfileTableBuilder::new().unwrap();
 }
 
 #[test]
-fn test_add_input() {
+fn add_input() {
     let mut vpt_builder = VariableProfileTableBuilder::new().unwrap();
     vpt_builder
         .add_input::<f32>("input", &[1, 3, 224, 224])
@@ -15,7 +15,7 @@ fn test_add_input() {
 
 #[test]
 #[should_panic(expected="InvalidDimsSize")]
-fn test_add_input_invalid_dims() {
+fn add_input_invalid_dims() {
     let mut vpt_builder = VariableProfileTableBuilder::new().unwrap();
     vpt_builder
         .add_input::<f32>("input", &[1, 3, 224])
@@ -24,7 +24,7 @@ fn test_add_input_invalid_dims() {
 
 #[test]
 #[should_panic(expected="NulError")]
-fn test_add_input_invalid_name() {
+fn add_input_invalid_name() {
     let mut vpt_builder = VariableProfileTableBuilder::new().unwrap();
     vpt_builder
         .add_input::<f32>("in\0put", &[1, 3, 224, 224])
@@ -32,14 +32,14 @@ fn test_add_input_invalid_name() {
 }
 
 #[test]
-fn test_add_output() {
+fn add_output() {
     let mut vpt_builder = VariableProfileTableBuilder::new().unwrap();
     vpt_builder.add_output::<f32>("output").unwrap();
 }
 
 #[test]
 #[should_panic(expected="NulError")]
-fn test_add_output_invalid_name() {
+fn add_output_invalid_name() {
     let mut vpt_builder = VariableProfileTableBuilder::new().unwrap();
     vpt_builder.add_output::<f32>("out\0put").unwrap();
 }
