e563f701b0f4fc1a2d9a2d1fed97ed7defc698c3
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-10 21:41:25 +0900

    remove test

--- menoh/src/model_data/tests.rs
+++ menoh/src/model_data/tests.rs
@@ -1,11 +1,6 @@
 use super::ModelData;
 
 #[test]
-fn from_onnx() {
-    ModelData::from_onnx("test.onnx").unwrap();
-}
-
-#[test]
 #[should_panic(expected="InvalidFilename")]
 fn from_onnx_invalid_path() {
     ModelData::from_onnx("invalid.onnx").unwrap();
