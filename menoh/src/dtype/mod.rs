d077123b834db6342b2679dfcb908d220d05b74d
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-10 10:33:30 +0900

    add doc for Dtype

--- menoh/src/dtype/mod.rs
+++ menoh/src/dtype/mod.rs
@@ -4,8 +4,27 @@ use Error;
 
 /// Trait representing scalar types supported by Menoh.
 pub unsafe trait Dtype {
+    /// Integer specifying the scalar type.
+    /// ```
+    /// extern crate menoh;
+    /// extern crate menoh_sys;
+    ///
+    /// use menoh::Dtype;
+    /// assert_eq!(f32::ID, menoh_sys::menoh_dtype_float as menoh_sys::menoh_dtype);
+    /// ```
     const ID: menoh_sys::menoh_dtype;
 
+    /// Verify a scalar type.
+    /// ```
+    /// use menoh::Dtype;
+    /// assert!(f32::check(f32::ID).is_ok());
+    /// ```
+    ///
+    /// # Failures
+    /// ```
+    /// use menoh::Dtype;
+    /// assert!(f32::check(f32::ID + 1).is_err());
+    /// ```
     fn check(dtype: menoh_sys::menoh_dtype) -> Result<(), Error> {
         if dtype == Self::ID {
             Ok(())
