1b0f502947b2c7b2d8c2f3c9c386ee43e218d60b
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-11 11:18:52 +0900

    contain bindings

--- menoh-sys/build.rs
+++ menoh-sys/build.rs
@@ -1,29 +1,8 @@
-extern crate bindgen;
 extern crate pkg_config;
 
-use std::env;
-use std::path;
-
 fn main() {
-    let library = pkg_config::Config::new()
+    pkg_config::Config::new()
         .atleast_version("1.0")
         .probe("menoh")
-        .unwrap();
-
-    let bindings = bindgen::Builder::default()
-        .clang_args(library
-                        .include_paths
-                        .iter()
-                        .map(|p| format!("-I{}", p.to_str().unwrap())))
-        .header_contents("wrapper.h", "#include <menoh/menoh.h>")
-        .whitelist_type("menoh_.*")
-        .whitelist_function("menoh_.*")
-        .whitelist_recursively(false)
-        .prepend_enum_name(false)
-        .rustfmt_bindings(false)
-        .generate()
-        .unwrap();
-    bindings
-        .write_to_file(path::PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
         .unwrap();
 }
