1c33faa9fd7307d0acf7e7f1571cd4ac4ea57da1
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-14 11:22:27 +0900

    suppress warning in MSVC

--- menoh-sys/build.rs
+++ menoh-sys/build.rs
@@ -4,10 +4,9 @@ fn main() {
     match pkg_config::Config::new()
               .atleast_version("1.0")
               .probe("menoh") {
-        Err(err) => {
-            println!("cargo:warning=pkg-config failed: {}", err);
-            println!("cargo:rustc-link-lib=dylib=menoh");
-        }
-        _ => (),
+        Ok(_) => return,
+        Err(pkg_config::Error::MSVC) => (),
+        Err(err) => println!("cargo:warning=pkg-config failed: {}", err),
     }
+    println!("cargo:rustc-link-lib=dylib=menoh");
 }
